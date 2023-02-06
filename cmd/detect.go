d691198253f69b51c929cbbf30ecac02fbd64806
lukeatayde <lucas.atayde@twosigma.com>
2023-02-06 15:16:43 -0500

    Add small comment for consistency

--- cmd/detect.go
+++ cmd/detect.go
@@ -57,19 +57,18 @@ func runDetect(cmd *cobra.Command, args []string) {
 	if err != nil {
 		log.Fatal().Err(err).Msg("")
 	}
-	source, err := cmd.Flags().GetString("source")
-	if err != nil {
-		log.Fatal().Err(err).Msg("")
-	}
 
 	// set verbose flag
 	if detector.Verbose, err = cmd.Flags().GetBool("verbose"); err != nil {
 		log.Fatal().Err(err).Msg("")
 	}
+
 	// set redact flag
 	if detector.Redact, err = cmd.Flags().GetBool("redact"); err != nil {
 		log.Fatal().Err(err).Msg("")
 	}
+
+	// set max target megabytes flag
 	if detector.MaxTargetMegaBytes, err = cmd.Flags().GetInt("max-target-megabytes"); err != nil {
 		log.Fatal().Err(err).Msg("")
 	}
